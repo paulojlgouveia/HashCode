cca81d46933eeabc84d32df284d72c31d53535c6
goncalochincho <goncalochincho@gmail.com>
2018-02-21 14:07:10 +0000

    first source with params parsing

---
+++ source.py
@@ -0,0 +1,13 @@
+
+
+file = open("paris_54000.txt",'r')
+input = file.readlines()
+params = input[0].split()
+N = int(params[0])
+M = int(params[1])
+T = int(params[2])
+C = int(params[3])
+S = int(params[4])
+junctions = input[1:N+1]
+streets = input[N+1:]
+
